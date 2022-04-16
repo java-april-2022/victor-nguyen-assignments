e2767ef417a16e1e516a23342154d47506357c74
Victor Nguyen <vln3dd@virginia.edu>
2022-04-15 23:59:21 -0400

    private, methods, constructors, get/setters

--- Week2/Extra Practice/Orders & Items/Item.java
+++ Week2/Extra Practice/Orders & Items/Item.java
@@ -1,6 +1,27 @@
 public class Item {
 
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
